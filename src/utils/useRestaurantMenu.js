297df44a8e9d30b40504a6d03832cec8e5ee8143
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-07 17:54:59 +0530

    fetch url changed

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -7,7 +7,9 @@ const useRestaurantMenu = (resId) => {
     fetchData();
   }, []);
   const fetchData = async () => {
-    const data = await fetch(MENU_URL + resId);
+    const data = await fetch(
+      `${MENU_URL}&lat=${lat}&lng=${lng}&restaurantId=${id}`
+    );
     console.log(data);
     const json = await data.json();
     console.log(json);
