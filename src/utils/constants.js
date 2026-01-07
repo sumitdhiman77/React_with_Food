58515d1428a4bc0c6413c96c4350ab08a103113a
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-07 17:18:16 +0530

    dapi added in path query

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,9 +4,9 @@ export const LOGO_URL =
 // export const MENU_URL =
 //   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=32.219042&lng=76.3234037&restaurantId=";
   export const MENU_URL =
-  "/api/swiggy-proxy?path=menu/pl&page-type=REGULAR_MENU&complete-menu=true&lat=32.219042&lng=76.3234037&restaurantId=";
+  "/api/swiggy-proxy?path=dapi/menu/pl&page-type=REGULAR_MENU&complete-menu=true&lat=32.219042&lng=76.3234037&restaurantId=";
   export const ExploreRestaurants_URL =
-  "/api/swiggy-proxy?path=dapi/restaurants/list/v5&lat=31.10340&lng=77.16590&collection=";
+  "/api/swiggy-proxy?path=restaurants/list/v5&lat=31.10340&lng=77.16590&collection=";
   // export const ExploreRestaurants_URL =
   //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.10340&lng=77.16590&collection=";
 export const ITEM_IMG_URL =
