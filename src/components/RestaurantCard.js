25f4c35886f369f4f68ac116f91be64464c859e8
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-08 18:17:38 +0530

    bug fixed

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,7 +1,6 @@
 import { CDN_URL } from "../utils/constants";
 const RestaurantCard = ({ resData }) => {
-  const { name, avgRating, cuisines, areaName, cloudinaryImageId } =
-    resData?.info;
+  const { name, avgRating, cuisines, areaName, cloudinaryImageId } = resData;
   return (
     <div className="truncate grid w-44 h-60 ml-16 ">
       <img
