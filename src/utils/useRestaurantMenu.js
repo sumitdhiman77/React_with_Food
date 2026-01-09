532c17ecea3805ed35e6f68aa40d26ffade0e73e
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-09 13:39:35 +0530

    remove bug

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -12,9 +12,9 @@ const useRestaurantMenu = (resId, lat, lng) => {
   const fetchMenu = async () => {
     try {
       const response = await fetch(
-        `https://proxy.corsfix.com/?${MENU_URL}&lat=${lat}&lng=${lng}&restaurantId=${resId}`
+        `https://proxy.corsfix.com/?${MENU_URL}`
       );
-
+// &lat=${lat}&lng=${lng}&restaurantId=${resId}
       if (!response.ok) {
         throw new Error("Menu fetch failed");
       }
