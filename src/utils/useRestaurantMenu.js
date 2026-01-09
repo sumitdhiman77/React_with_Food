409a7882034545d1e09d1caad2a0c7616ca273fa
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-09 12:53:29 +0530

    changes

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -12,8 +12,7 @@ const useRestaurantMenu = (resId, lat, lng) => {
   const fetchMenu = async () => {
     try {
       const response = await fetch(
-        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=32.219042&lng=76.26450&restaurantId=917055"
-        // `${MENU_URL}&lat=${lat}&lng=${lng}&restaurantId=${resId}`
+        `${MENU_URL}&lat=${lat}&lng=${lng}&restaurantId=${resId}`
       );
 
       if (!response.ok) {
