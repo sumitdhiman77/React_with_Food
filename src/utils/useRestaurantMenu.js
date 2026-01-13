3c2dada5c19988529f1837baa3cbba3d22361ba4
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-13 14:46:12 +0530

    respnse handle

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,15 +1,15 @@
 // src/utils/useRestaurantMenu.js
 import { useState, useEffect } from "react";
-import menu from "../data/207611.json"; // Import JSON directly
 
 const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState(null);
 
   useEffect(() => {
-    if (!resId) return;
-
+    fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=32.08830&lng=76.26450&restaurantId=820385&catalog_qa=undefined&query=Pizza&submitAction=ENTER"
+    );
     // Since JSON is imported, we can filter if needed
-    if (menu.data && resId === "207611") {
+    if (menu.data && resId === "820385") {
       setResInfo(menu.data);
     }
   }, [resId]);
