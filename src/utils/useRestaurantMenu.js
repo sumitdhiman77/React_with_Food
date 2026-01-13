58c8e4f130eea7232a1bb529a31ad5c7ec6c24dd
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-13 17:56:16 +0530

    changes

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -8,7 +8,6 @@ const useRestaurantMenu = (resId, lat, lng, query) => {
       const res = await fetch(
         `${MENU_URL}&lat=${lat}&lng=${lng}&restaurantId=${resId}&catalog_qa=undefined&query=${query}&submitAction=ENTER`
       );
-      // "/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=32.08830&lng=76.26450&restaurantId=820385&catalog_qa=undefined&query=Pizza&submitAction=ENTER"
       const resData = await res.json();
       if (!resData?.data?.cards) {
         console.error("Invalid Swiggy response", json);
