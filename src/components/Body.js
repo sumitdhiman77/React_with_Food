c48559406262d5493d977eedc0857a5291c19614
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-28 20:05:57 +0530

    change

--- src/components/Body.js
+++ src/components/Body.js
@@ -52,7 +52,7 @@ const Body = () => {
   const fetchData = async () => {
     try {
       const res = await fetch(
-        `${ExploreRestaurants_URL}&lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`,
+        `${ExploreRestaurants_URL}?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`,
       );
       const json = await res.json();
       if (!json?.data?.data?.cards) {
