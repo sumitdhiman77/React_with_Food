1d78e1b4bbe4588c643a68b7b7bd95269b688f5d
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-01-31 20:40:38 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,5 +14,3 @@ export const ITEM_IMG_URL =
 
 export const EXPLORE_IMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
-
-("https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/57429af1-4bbf-44e9-a0cf-597c8b83178c_118350.JPG");
