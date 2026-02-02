e072bdfb3932642a1104b6155bffe95884aa10e0
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-02 15:46:25 +0530

    set header

--- api/swiggy-proxy.js
+++ api/swiggy-proxy.js
@@ -32,6 +32,8 @@ export default async function handler(req, res) {
         Referer: "https://www.swiggy.com",
         Origin: "https://www.swiggy.com",
         "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
+        cookie:
+          "__SW=01ByxBgWWiJkxPkV8qFDMS_Ubc3hd5hD; _device_id=21586c2f-0280-981d-9ddf-e8842937269e; fontsLoaded=1; _gcl_au=1.1.1977291906.1769598828; userLocation={%22lat%22:32.2182266%2C%22lng%22:76.3172673%2C%22address%22:%226898+8MV%2C%20Sudher%2C%20Dharamshala%2C%20Himachal%20Pradesh%20176215%2C%20India%22%2C%22area%22:%22%22%2C%22showUserDefaultAddressHint%22:false}; _guest_tid=576ab4d0-6698-4239-b9d6-3682ba83ddc8; _sid=ph16a6fe-c723-4a07-b051-2c0cf74a68fc; _gid=GA1.2.2088096975.1770019393; _gat_0=1; _ga=GA1.1.1397054701.1769598830; _ga_34JYJ0BCRN=GS2.1.s1770019397$o12$g1$t1770027307$j60$l0$h0; _ga_YE38MFJRBZ=GS2.1.s1770019398$o12$g1$t1770027308$j60$l0$h0; aws-waf-token=b57e8fd0-58f3-4404-9790-07751a60c0a8:BQoAs9ZGTsJaAAAA:pFXAwwX1I44LAxRLSUikYVWyAqlgCfmDIInT0neyT0CC8avHSJBOc+Fdgo9NsiltrTFkywUS0zoGgetw8t49jt7BK0eU99MlTE30ZO9IcsSK84e0UVknIwaVDjlc+CHS5drnGT+tqwPov2Xk+Kj41zaiJJxgwfCR5GOpv6SA0xFiWaGOQGw+XyaHFLx8l4OdNlANiQrDWvj0L7jbjBhDvTcHTr9t5jW+66yzEy+XxqLVN4FlOI3dkVDf6Qw=",
 
         // 4. The "Last Resort" (Manually copy your aws-waf-token if the above fails)
         // "Cookie": "aws-waf-token=PASTE_YOUR_TOKEN_HERE; _device_id=21586c2f-..."
