746ab784a07e5f85d051a25dfca7869371f17c07
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-03 13:07:59 +0530

    console

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -5,19 +5,15 @@ const useRestaurantMenu = (resId, lat, lng, query) => {
 
   const fetchData = async () => {
     try {
-      const res = await fetch(
-        `${MENU_URL}&page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${resId}&catalog_qa=undefined&query=${query}&submitAction=ENTER`,
-      );
-      let resData = await res.json();
-      if (!resData?.data?.cards) {
-        resData = await fetch(`/mock-menus/${query}.json}`);
-        console.log("resData is:", resData);
-        // console.error("Invalid Swiggy response:", resData);
-        // return;
+      const res = await fetch(`/mock-menus/${query}.json`);
+      // `${MENU_URL}&page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${resId}&catalog_qa=undefined&query=${query}&submitAction=ENTER`,
+      if (!res.ok) {
+        console.log("file missing or wrong file name");
+        return;
       }
+      const resData = await res.json();
       console.log("in useRestaurantMenu resData is", resData);
       setResInfo(resData);
-      console.log(resInfo);
     } catch (error) {
       console.error("Fetch error:", error);
     }
@@ -26,6 +22,7 @@ const useRestaurantMenu = (resId, lat, lng, query) => {
   useEffect(() => {
     if (!lat || !lng) return;
     fetchData();
+    console.log(resInfo);
   }, [lat, lng]);
 };
 
