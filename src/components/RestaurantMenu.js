97f4812f9fdc578635096135a8fb4fbb78195b18
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-15 14:15:02 +0530

    changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -15,8 +15,9 @@ const RestaurantMenu = () => {
   const { lat, lng } = useContext(LocationContext);
   const [item, setItem] = useState(null);
   const [veg, setVeg] = useState(false);
-  const { name, query } = useParams();
+  let { name, query } = useParams();
   console.log("name is:", name, "query is:", query);
+  name = decodeURIComponent(name);
   const resInfo = useRestaurantMenu(name, lat, lng, query);
   console.log("resInfo is:", resInfo);
   if (!resInfo) return <Shimmer />;
